f7cb5aeb2f6e301e8a91855d51a9a1b14b466019
0xshiba1 <158560741+0xshiba1@users.noreply.github.com>
2024-10-28 12:45:58 +0800

    disable sourcemaps

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -48,7 +48,12 @@ module.exports = withSentryConfig(module.exports, {
   // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
   // Upload a larger set of source maps for prettier stack traces (increases build time)
-  widenClientFileUpload: false, // TODO: Revert to true
+  widenClientFileUpload: true,
+
+  // TODO: REMOVE once Sentry works again
+  sourcemaps: {
+    disable: true,
+  },
 
   // Automatically annotate React components to show their full name in breadcrumbs and session replay
   reactComponentAnnotation: {
