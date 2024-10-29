ca9b6473e5de77f8008b4cc038558263671bf0f4
0xshiba1 <158560741+0xshiba1@users.noreply.github.com>
2024-10-29 13:36:47 +0800

    remove unused wormhole copy

--- frontend/public/wormhole-connect@1.1.2/assets/events-DXt1Ggbt.js
+++
@@ -1,1 +0,0 @@
-import{P as s,s as e}from"../main.js";var r=s();const a=e(r);export{a,r as e};
--- frontend/public/wormhole-connect@1.1.2/scripts/ofac/utils.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const getSanctionedAddresses: () => Promise<string[]>;
-//# sourceMappingURL=utils.d.ts.map
