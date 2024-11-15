b8166718cc15ff7da99c8f3ad52b290ac0c6d338
0xshiba1 <158560741+0xshiba1@users.noreply.github.com>
2024-11-15 15:40:36 +0800

    update scripts and liquidator deps

---
+++ liquidator/eslint.config.js
@@ -0,0 +1,77 @@
+const { FlatCompat } = require("@eslint/eslintrc");
+const js = require("@eslint/js");
+const prettier = require("eslint-plugin-prettier");
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+module.exports = [
+  { ignores: ["**/_generated"] },
+  ...compat.extends(
+    "prettier",
+    "next/core-web-vitals",
+    "plugin:@typescript-eslint/recommended"
+  ),
+  {
+    plugins: {
+      prettier,
+    },
+
+    rules: {
+      "react-hooks/exhaustive-deps": "error",
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-require-imports": "off",
+      "prettier/prettier": ["error"],
+      "import/order": [
+        "error",
+        {
+          pathGroups: [
+            {
+              pattern: "*(react|next)/**",
+              patternOptions: { partial: true },
+              group: "external",
+              position: "before",
+            },
+            {
+              pattern: "@suilend/**",
+              patternOptions: { partial: true },
+              group: "internal",
+              position: "before",
+            },
+            {
+              pattern: "@/*/**",
+              group: "internal",
+              position: "before",
+            },
+          ],
+          pathGroupsExcludedImportTypes: [],
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            "parent",
+            "sibling",
+            "index",
+            "object",
+            "type",
+          ],
+          "newlines-between": "always",
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+        },
+      ],
+      "sort-imports": [
+        "error",
+        {
+          ignoreDeclarationSort: true,
+        },
+      ],
+    },
+  },
+];
