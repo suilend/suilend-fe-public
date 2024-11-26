9626af8c6e031ca4b492ba10985d0681d94c4e13
0xshiba1 <158560741+0xshiba1@users.noreply.github.com>
2024-11-26 08:37:18 +0800

    landing page ticker constant speed

--- frontend/src/components/shared/Ticker.tsx
+++ frontend/src/components/shared/Ticker.tsx
@@ -2,6 +2,12 @@ import { Fragment, ReactNode } from "react";
 
 import { ClassValue } from "clsx";
 
+import {
+  NON_SPONSORED_PYTH_PRICE_FEED_COINTYPES,
+  isInMsafeApp,
+} from "@suilend/frontend-sui";
+
+import { useLoadedAppContext } from "@/contexts/AppContext";
 import { cn } from "@/lib/utils";
 
 interface TickerProps {
@@ -10,6 +16,16 @@ interface TickerProps {
 }
 
 export default function Ticker({ className, items }: TickerProps) {
+  const { data } = useLoadedAppContext();
+
+  const animationDuration = `${
+    data.lendingMarket.reserves.filter((reserve) =>
+      !isInMsafeApp()
+        ? true
+        : !NON_SPONSORED_PYTH_PRICE_FEED_COINTYPES.includes(reserve.coinType),
+    ).length * 3
+  }s`;
+
   return (
     <div
       className={cn(
@@ -17,12 +33,18 @@ export default function Ticker({ className, items }: TickerProps) {
         className,
       )}
     >
-      <div className="flex w-max shrink-0 animate-infinite-scroll flex-row items-center gap-20 pr-20 md:gap-40 md:pr-40 lg:gap-60 lg:pr-60">
+      <div
+        className="flex w-max shrink-0 animate-infinite-scroll flex-row items-center gap-20 pr-20 md:gap-40 md:pr-40 lg:gap-60 lg:pr-60"
+        style={{ animationDuration }}
+      >
         {items.map((item, index) => (
           <Fragment key={index}>{item}</Fragment>
         ))}
       </div>
-      <div className="flex w-max shrink-0 animate-infinite-scroll flex-row items-center gap-20 pr-20 md:gap-40 md:pr-40 lg:gap-60 lg:pr-60">
+      <div
+        className="flex w-max shrink-0 animate-infinite-scroll flex-row items-center gap-20 pr-20 md:gap-40 md:pr-40 lg:gap-60 lg:pr-60"
+        style={{ animationDuration }}
+      >
         {items.map((item, index) => (
           <Fragment key={index}>{item}</Fragment>
         ))}
