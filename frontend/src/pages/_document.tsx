5c99cd3664a9f2f0549cc9a427175205bce7ddf3
0xshiba1 <158560741+0xshiba1@users.noreply.github.com>
2024-11-28 17:00:55 +0800

    update favicon

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -7,13 +7,13 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta name="description" content={DESCRIPTION} />
-        <link rel="icon" href="/android-chrome-384x384.png" />
+        <link rel="icon" href="/android-chrome-512x512.png" />
         <meta property="og:description" content={DESCRIPTION} />
         <meta property="og:type" content="website" />
         <meta property="og:title" content={TITLE} />
         <meta
           property="og:image"
-          content="https://www.suilend.fi/android-chrome-384x384.png"
+          content="https://www.suilend.fi/android-chrome-512x512.png"
         />
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:site" content={TWITTER} />
@@ -36,8 +36,6 @@ export default function Document() {
           href="/favicon-16x16.png"
         />
         <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-        <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="theme-color" content="#020A19" />
 
         {/* Wormhole Connect */}
