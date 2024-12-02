96e22174a4de17905f89250dc6438961148101aa
0xshiba1 <158560741+0xshiba1@users.noreply.github.com>
2024-12-02 19:06:34 +0800

    add suiet script

--- scripts/src/suiet.ts
+++ scripts/src/suiet.ts
@@ -4,7 +4,10 @@ import { SuiClient, SuiEvent } from "@mysten/sui/client";
 import { normalizeStructTag } from "@mysten/sui/utils";
 import BigNumber from "bignumber.js";
 
-import { NORMALIZED_SUI_COINTYPE } from "@suilend/frontend-sui";
+import {
+  NORMALIZED_SUI_COINTYPE,
+  NORMALIZED_sSUI_COINTYPE,
+} from "@suilend/frontend-sui";
 
 const minTimestampMs = 1732528800000; // 25 Nov 10AM UTC
 const maxTimestampMs = 1732881600000; // 29 Nov 12PM UTC
@@ -102,10 +105,7 @@ const getBorrowAddresses = async (suiClient: SuiClient) => {
     .map(([address]) => address);
   console.log("sendersWith5PlusSuiBorrowed", sendersWith5PlusSuiBorrowed);
 
-  fs.writeFileSync(
-    "senders-with-5-plus-sui-borrowed.json",
-    JSON.stringify(sendersWith5PlusSuiBorrowed),
-  );
+  return sendersWith5PlusSuiBorrowed;
 };
 
 const getStakeAndDepositSsuiAddresses = async (suiClient: SuiClient) => {
@@ -135,6 +135,66 @@ const getStakeAndDepositSsuiAddresses = async (suiClient: SuiClient) => {
   ) as SuiEvent[];
   console.log("mintEvents.length", mintEvents.length);
   console.log("depositEvents.length", depositEvents.length);
+
+  // Filter events
+  const ssuiMintEvents = mintEvents.filter(
+    (event) =>
+      normalizeStructTag((event.parsedJson as any).event.typename.name) ===
+      NORMALIZED_sSUI_COINTYPE,
+  );
+  console.log("ssuiMintEvents.length", ssuiMintEvents.length);
+
+  const ssuiMintEventsWithCorrespondingDepositEvents = ssuiMintEvents.filter(
+    (mintEvent) =>
+      !!depositEvents.find(
+        (depositEvent) => depositEvent.id.txDigest === mintEvent.id.txDigest,
+      ),
+  );
+  console.log(
+    "ssuiMintEventsWithCorrespondingDepositEvents.length",
+    ssuiMintEventsWithCorrespondingDepositEvents.length,
+  );
+
+  const distinctSenders = ssuiMintEventsWithCorrespondingDepositEvents.reduce(
+    (acc, event) =>
+      !acc.includes(event.sender) ? [...acc, event.sender] : acc,
+    [] as string[],
+  );
+  console.log("distinctSenders.length", distinctSenders.length);
+
+  const senderSuiStakedAndDepositedMap = distinctSenders.reduce(
+    (acc, sender) => ({
+      ...acc,
+      [sender]: +ssuiMintEventsWithCorrespondingDepositEvents
+        .filter((event) => event.sender === sender)
+        .reduce(
+          (acc, event) =>
+            acc.plus(
+              new BigNumber((event.parsedJson as any).event.sui_amount_in).div(
+                10 ** 9,
+              ),
+            ),
+          new BigNumber(0),
+        ),
+    }),
+    {} as Record<string, number>,
+  );
+  console.log(
+    "senderSuiStakedAndDepositedMap",
+    senderSuiStakedAndDepositedMap,
+  );
+
+  const sendersWith10PlusSuiStakedAndDeposited = Object.entries(
+    senderSuiStakedAndDepositedMap,
+  )
+    .filter(([address, amount]) => amount >= 10)
+    .map(([address]) => address);
+  console.log(
+    "sendersWith10PlusSuiStakedAndDeposited",
+    sendersWith10PlusSuiStakedAndDeposited,
+  );
+
+  return sendersWith10PlusSuiStakedAndDeposited;
 };
 
 async function main() {
@@ -144,7 +204,44 @@ async function main() {
     }`,
   });
 
-  // await getBorrowAddresses(suiClient);
-  await getStakeAndDepositSsuiAddresses(suiClient);
+  const sendersWith5PlusSuiBorrowed = await getBorrowAddresses(suiClient);
+  fs.writeFileSync(
+    "senders-with-5-plus-sui-borrowed.csv",
+    `address\n${sendersWith5PlusSuiBorrowed.join("\n")}`,
+  );
+
+  const sendersWith10PlusSuiStakedAndDeposited =
+    await getStakeAndDepositSsuiAddresses(suiClient);
+  fs.writeFileSync(
+    "senders-with-10-plus-sui-staked-and-deposited.csv",
+    `address\n${sendersWith10PlusSuiStakedAndDeposited.join("\n")}`,
+  );
+
+  const eligibleSenders = [];
+  for (const sender of Array.from(
+    new Set([
+      ...sendersWith5PlusSuiBorrowed,
+      ...sendersWith10PlusSuiStakedAndDeposited,
+    ]),
+  )) {
+    if (
+      sendersWith5PlusSuiBorrowed.includes(sender) &&
+      sendersWith10PlusSuiStakedAndDeposited.includes(sender)
+    )
+      eligibleSenders.push(sender);
+  }
+  console.log(
+    "sendersWith5PlusSuiBorrowed.length",
+    sendersWith5PlusSuiBorrowed.length,
+    "sendersWith10PlusSuiStakedAndDeposited.length",
+    sendersWith10PlusSuiStakedAndDeposited.length,
+    "eligibleSenders.length",
+    eligibleSenders.length,
+  );
+
+  fs.writeFileSync(
+    "suiet-eligible-addresses.csv",
+    `address\n${eligibleSenders.join("\n")}`,
+  );
 }
 main();
