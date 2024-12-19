51d3ca081860e138c8395c4f6965984a68342c1a
0xshiba1 <158560741+0xshiba1@users.noreply.github.com>
2024-12-19 13:40:36 +0800

    move seo files to s3

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -1,41 +1,44 @@
 import { Head, Html, Main, NextScript } from "next/document";
 
-import { DESCRIPTION, TITLE, TWITTER } from "@/lib/constants";
+import { ASSETS_URL, DESCRIPTION, TITLE, TWITTER } from "@/lib/constants";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
         <meta name="description" content={DESCRIPTION} />
-        <link rel="icon" href="/android-chrome-512x512.png" />
+        <link
+          rel="icon"
+          href={`${ASSETS_URL}/seo/android-chrome-512x512.png`}
+        />
         <meta property="og:description" content={DESCRIPTION} />
         <meta property="og:type" content="website" />
         <meta property="og:title" content={TITLE} />
         <meta
           property="og:image"
-          content="https://www.suilend.fi/android-chrome-512x512.png"
+          content={`${ASSETS_URL}/seo/android-chrome-512x512.png`}
         />
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:site" content={TWITTER} />
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href="/apple-touch-icon.png"
+          href={`${ASSETS_URL}/seo/apple-touch-icon.png`}
         />
         <link
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href="/favicon-32x32.png"
+          href={`${ASSETS_URL}/seo/favicon-32x32.png`}
         />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href={`${ASSETS_URL}/seo/favicon.ico`} />
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href="/favicon-16x16.png"
+          href={`${ASSETS_URL}/seo/favicon-16x16.png`}
         />
-        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="manifest" href={`${ASSETS_URL}/seo/site.webmanifest`} />
         <meta name="theme-color" content="#020A19" />
 
         {/* Wormhole Connect */}
