23a145947657b591176b8aceafeb948dd19b7e83
0xshiba1 <158560741+0xshiba1@users.noreply.github.com>
2025-01-09 11:40:55 +0800

    deprecated assets separate table

--- frontend/src/components/dashboard/market-table/MarketCardList.tsx
+++ frontend/src/components/dashboard/market-table/MarketCardList.tsx
@@ -26,10 +26,7 @@ import {
   TTitle,
 } from "@/components/shared/Typography";
 import { Separator } from "@/components/ui/separator";
-import {
-  ISOLATED_TOOLTIP,
-  OPEN_LTV_BORROW_WEIGHT_TOOLTIP,
-} from "@/lib/tooltips";
+import { OPEN_LTV_BORROW_WEIGHT_TOOLTIP } from "@/lib/tooltips";
 import { cn, hoverUnderlineClassName } from "@/lib/utils";
 
 export enum MarketCardListType {
@@ -99,109 +96,140 @@ interface MarketCardListProps {
 export default function MarketCardList({ rows }: MarketCardListProps) {
   const { open: openActionsModal } = useActionsModalContext();
 
+  const [headerRowIsExpandedMap, setHeaderRowIsExpandedMap] = useState<
+    Record<string, boolean>
+  >({});
   const [collapsibleRowIsExpandedMap, setCollapsibleRowIsExpandedMap] =
     useState<Record<string, boolean>>({});
 
   return (
     <div className="flex w-full flex-col gap-6">
       {rows.map((row, index) => {
-        const { isIsolated, count } = row;
+        const { isDeprecated, title, tooltip, count } = row;
+
+        const isHeaderRowExpanded =
+          !isDeprecated || headerRowIsExpandedMap[title];
+        const HeaderRowIcon = isHeaderRowExpanded ? ChevronUp : ChevronDown;
 
         return (
           <div key={index} className="flex w-full flex-col gap-4">
-            {/* Heading */}
-            <div className="flex flex-row items-center gap-2">
-              <Tooltip title={isIsolated ? ISOLATED_TOOLTIP : undefined}>
+            {/* Title */}
+            <button
+              className="group flex flex-row items-center gap-2"
+              onClick={
+                !isDeprecated
+                  ? undefined
+                  : () =>
+                      setHeaderRowIsExpandedMap((prev) => ({
+                        ...prev,
+                        [title]: !prev[title],
+                      }))
+              }
+            >
+              {isDeprecated && (
+                <HeaderRowIcon className="-mr-1 h-4 w-4 text-primary" />
+              )}
+
+              <Tooltip title={tooltip}>
                 <TTitle
                   className={cn(
                     "w-max uppercase",
-                    isIsolated &&
+                    !!tooltip &&
                       cn("decoration-primary/50", hoverUnderlineClassName),
                   )}
                 >
-                  {isIsolated ? "Isolated" : "Main"} assets
+                  {title}
                 </TTitle>
               </Tooltip>
               <TLabel>{count}</TLabel>
-            </div>
+            </button>
 
             {/* Cards */}
-            <div className="flex w-full flex-col gap-2">
-              {row.subRows.map((subRow, index2) => {
-                if ((subRow as CollapsibleRowData).isCollapsibleRow) {
-                  const { title, subRows } = subRow as CollapsibleRowData;
+            {isHeaderRowExpanded && (
+              <div className="flex w-full flex-col gap-2">
+                {row.subRows.map((subRow, index2) => {
+                  if ((subRow as CollapsibleRowData).isCollapsibleRow) {
+                    const { title, subRows } = subRow as CollapsibleRowData;
 
-                  const isExpanded = collapsibleRowIsExpandedMap[title];
-                  const Icon = isExpanded ? ChevronUp : ChevronDown;
+                    const isCollapsibleRowExpanded =
+                      collapsibleRowIsExpandedMap[title];
+                    const CollapsibleRowIcon = isCollapsibleRowExpanded
+                      ? ChevronUp
+                      : ChevronDown;
 
-                  return (
-                    <div
-                      key={title}
-                      className={cn(
-                        "-mx-4 flex flex-col gap-4 px-4",
-                        (index2 !== 0 || isExpanded) && "pt-2",
-                        (index2 !== row.subRows.length - 1 || isExpanded) &&
-                          "pb-2",
-                        isExpanded &&
-                          "bg-gradient-to-r from-muted/5 to-muted/0 shadow-[inset_2px_0_0_0px_hsl(var(--primary))]",
-                      )}
-                    >
-                      {/* Title */}
-                      <button
-                        className="flex flex-row items-center gap-3"
-                        onClick={() =>
-                          setCollapsibleRowIsExpandedMap((prev) => ({
-                            ...prev,
-                            [title]: !prev[title],
-                          }))
-                        }
+                    return (
+                      <div
+                        key={title}
+                        className={cn(
+                          "-mx-4 flex flex-col gap-4 px-4",
+                          (index2 !== 0 || isCollapsibleRowExpanded) && "pt-2",
+                          (index2 !== row.subRows.length - 1 ||
+                            isCollapsibleRowExpanded) &&
+                            "pb-2",
+                          isCollapsibleRowExpanded &&
+                            "bg-gradient-to-r from-muted/5 to-muted/0 shadow-[inset_2px_0_0_0px_hsl(var(--primary))]",
+                        )}
                       >
-                        <div className="flex h-7 w-7 flex-row items-center justify-center rounded-md bg-muted/15">
-                          <Icon className="h-5 w-5 text-foreground" />
-                        </div>
-
-                        <div className="flex min-w-max flex-col gap-1">
-                          <div className="flex flex-row items-center gap-2">
-                            <TBody>{title}</TBody>
-                            <TLabel>{subRows.length}</TLabel>
+                        {/* Title */}
+                        <button
+                          className="flex flex-row items-center gap-3"
+                          onClick={() =>
+                            setCollapsibleRowIsExpandedMap((prev) => ({
+                              ...prev,
+                              [title]: !prev[title],
+                            }))
+                          }
+                        >
+                          <div className="flex h-7 w-7 flex-row items-center justify-center rounded-md bg-muted/15">
+                            <CollapsibleRowIcon className="h-5 w-5 text-foreground" />
                           </div>
 
-                          <TokenLogos
-                            className="h-4 w-4"
-                            tokens={subRows.map((subRow) => subRow.token)}
-                          />
-                        </div>
-                      </button>
+                          <div className="flex min-w-max flex-col gap-1">
+                            <div className="flex flex-row items-center gap-2">
+                              <TBody>{title}</TBody>
+                              <TLabel>{subRows.length}</TLabel>
+                            </div>
 
-                      {/* Cards */}
-                      {isExpanded && (
-                        <div className="flex w-full flex-col gap-2">
-                          {subRows.map((subSubRow) => (
-                            <MarketCard
-                              key={subSubRow.token.coinType}
-                              rowData={subSubRow}
-                              onClick={() =>
-                                openActionsModal(subSubRow.token.symbol)
-                              }
+                            <TokenLogos
+                              className="h-4 w-4"
+                              tokens={subRows.map((subRow) => subRow.token)}
                             />
-                          ))}
-                        </div>
-                      )}
-                    </div>
-                  );
-                }
+                          </div>
+                        </button>
 
-                return (
-                  <MarketCard
-                    key={(subRow as ReservesRowData).token.coinType}
-                    rowData={subRow as ReservesRowData}
-                    onClick={() =>
-                      openActionsModal((subRow as ReservesRowData).token.symbol)
-                    }
-                  />
-                );
-              })}
-            </div>
+                        {/* Cards */}
+                        {isCollapsibleRowExpanded && (
+                          <div className="flex w-full flex-col gap-2">
+                            {subRows.map((subSubRow) => (
+                              <MarketCard
+                                key={subSubRow.token.coinType}
+                                rowData={subSubRow}
+                                onClick={() =>
+                                  openActionsModal(subSubRow.token.symbol)
+                                }
+                              />
+                            ))}
+                          </div>
+                        )}
+                      </div>
+                    );
+                  }
+
+                  // Card
+                  return (
+                    <MarketCard
+                      key={(subRow as ReservesRowData).token.coinType}
+                      rowData={subRow as ReservesRowData}
+                      onClick={() =>
+                        openActionsModal(
+                          (subRow as ReservesRowData).token.symbol,
+                        )
+                      }
+                    />
+                  );
+                })}
+              </div>
+            )}
           </div>
         );
       })}
