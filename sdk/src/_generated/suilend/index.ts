eb875ce69017996146ff551b60ffee347623c67f
0xripleys <0xripleys@solend.fi>
2025-01-15 17:10:47 -0500

    wrong mainnet id

--- sdk/src/_generated/suilend/index.ts
+++ sdk/src/_generated/suilend/index.ts
@@ -3,7 +3,7 @@ export const PACKAGE_ID = process.env.NEXT_PUBLIC_USE_BETA_MARKET
   : "0xf95b06141ed4a174f239417323bde3f209b972f5930d8521ea38a52aff3a6ddf";
 export let PUBLISHED_AT = process.env.NEXT_PUBLIC_USE_BETA_MARKET
   ? "0xe5ed361add4433f4d23e56fc0e3bacab39b93592d5e65d508e33fd19ff696669"
-  : "0x465174d80d10d33917d16721d20e6a4e08c50e9d3ff9115ee8c36cf9fb7bcd6e";
+  : "0xe37cc7bb50fd9b6dbd3873df66fa2c554e973697f50ef97707311dc78bd08444";
 export const PKG_V1 = process.env.NEXT_PUBLIC_USE_BETA_MARKET
   ? "0x1f54a9a2d71799553197e9ea24557797c6398d6a65f2d4d3818c9304b75d5e21"
   : "0xf95b06141ed4a174f239417323bde3f209b972f5930d8521ea38a52aff3a6ddf";
@@ -12,7 +12,7 @@ export const PKG_V8 = process.env.NEXT_PUBLIC_USE_BETA_MARKET
   : "0x5b54b47971238403d6ade3c8c2cc75814cb55145a5184af916bb5b12aaf184cb";
 export const PKG_V10 = process.env.NEXT_PUBLIC_USE_BETA_MARKET
   ? "0xe5ed361add4433f4d23e56fc0e3bacab39b93592d5e65d508e33fd19ff696669"
-  : "0x465174d80d10d33917d16721d20e6a4e08c50e9d3ff9115ee8c36cf9fb7bcd6e";
+  : "0xe37cc7bb50fd9b6dbd3873df66fa2c554e973697f50ef97707311dc78bd08444";
 
 export function setPublishedAt(publishedAt: string) {
   PUBLISHED_AT = publishedAt;
