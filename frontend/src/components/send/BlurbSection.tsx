223a50594c5c280f3a099f59a4e9d3d474b8e23b
0xshiba1 <158560741+0xshiba1@users.noreply.github.com>
2025-02-10 10:20:23 +0800

    capitalize STEAMM in blurb

--- frontend/src/components/send/BlurbSection.tsx
+++ frontend/src/components/send/BlurbSection.tsx
@@ -27,7 +27,7 @@ export default function BlurbSection() {
 
         <TBodySans className="leading-5">
           {
-            "Suilend launched in March 2024 and has quickly become a cornerstone of DeFi on Sui. We're now expanding with the Sui DeFi Suite—a complete lineup of products including  lending, infinite liquid staking with SpringSui and Steamm, our superfluid AMM. At the center of it all is SEND, the token representing our mission and vision. Backed by leading investors, we're ready to take Sui DeFi to the next level. Let's SEND IT!"
+            "Suilend launched in March 2024 and has quickly become a cornerstone of DeFi on Sui. We're now expanding with the Sui DeFi Suite—a complete lineup of products including  lending, infinite liquid staking with SpringSui and STEAMM, our superfluid AMM. At the center of it all is SEND, the token representing our mission and vision. Backed by leading investors, we're ready to take Sui DeFi to the next level. Let's SEND IT!"
           }
         </TBodySans>
 
