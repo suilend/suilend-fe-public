fda13f04d0eac61271754e19bd6c0cb48306f84f
0xshiba1 <158560741+0xshiba1@users.noreply.github.com>
2025-05-14 10:29:00 +0800

    add copy to clipboard

--- frontend/src/components/admin/AdminTokenSelectionDialog.tsx
+++ frontend/src/components/admin/AdminTokenSelectionDialog.tsx
@@ -1,6 +1,7 @@
-import { Token, getToken } from "@suilend/frontend-sui";
+import { SUI_COINTYPE, Token, getToken, isSui } from "@suilend/frontend-sui";
 import { useSettingsContext } from "@suilend/frontend-sui-next";
 
+import CopyToClipboardButton from "@/components/shared/CopyToClipboardButton";
 import OpenOnExplorerButton from "@/components/shared/OpenOnExplorerButton";
 import { TLabelSans } from "@/components/shared/Typography";
 import TokenSelectionDialog from "@/components/TokenSelectionDialog";
@@ -20,10 +21,21 @@ export default function AdminTokenSelectionDialog({
 
   return (
     <div className="flex flex-col gap-2">
-      <div className="flex h-4 flex-row items-center gap-1">
+      <div className="flex flex-row items-center gap-1">
         <TLabelSans>coin</TLabelSans>
         {token && (
-          <OpenOnExplorerButton url={explorer.buildCoinUrl(token.coinType)} />
+          <div className="flex h-4 flex-row items-center">
+            <CopyToClipboardButton
+              className="h-6 w-6 hover:bg-transparent"
+              iconClassName="w-3 h-3"
+              value={isSui(token.coinType) ? SUI_COINTYPE : token.coinType}
+            />
+            <OpenOnExplorerButton
+              className="h-6 w-6 hover:bg-transparent"
+              iconClassName="w-3 h-3"
+              url={explorer.buildCoinUrl(token.coinType)}
+            />
+          </div>
         )}
       </div>
 
