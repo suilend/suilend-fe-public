8eea409e62422ec76cfc8e6d8c011ea48d3b384d
0xshiba1 <158560741+0xshiba1@users.noreply.github.com>
2025-09-01 12:37:11 +0800

    fix build.sh

--- frontend/build.sh
+++ frontend/build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-echo "Checking if send has changed..."
+echo "Checking if frontend has changed..."
 
-# If send has changes in the last commit range, proceed with build
-if git diff --quiet HEAD^ HEAD -- .; then
-  echo "No changes in send/. Skipping build."
+# If frontend has changes in the last commit range, proceed with build
+if git diff --quiet HEAD^ HEAD -- . ../sdk/ ../.gitignore ../bun.lock ../eslint.config.js ../package.json ../README.md; then
+  echo "No changes in frontend/. Skipping build."
   exit 0
 else
-  echo "Changes detected in send/. Proceeding with build."
+  echo "Changes detected in frontend/. Proceeding with build."
   exit 1
 fi
