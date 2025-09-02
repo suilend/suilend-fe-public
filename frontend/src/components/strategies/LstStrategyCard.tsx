9b9464f57808c00e3c02007e64ca67f0701b1579
0xshiba1 <158560741+0xshiba1@users.noreply.github.com>
2025-09-02 14:24:16 +0800

    update card design

--- frontend/src/components/strategies/LstStrategyCard.tsx
+++ frontend/src/components/strategies/LstStrategyCard.tsx
@@ -20,6 +20,7 @@ import { Separator } from "@/components/ui/separator";
 import { useLoadedLstStrategyContext } from "@/contexts/LstStrategyContext";
 import { useLoadedUserContext } from "@/contexts/UserContext";
 import useHistoricalTvlAmountMap from "@/hooks/useHistoricalTvlAmountMap";
+import { ASSETS_URL } from "@/lib/constants";
 import { cn } from "@/lib/utils";
 
 interface LstStrategyCardProps {
@@ -181,165 +182,179 @@ export default function LstStrategyCard({
 
   return (
     <div
-      className="flex w-full cursor-pointer flex-col gap-4 rounded-sm border bg-card p-4 transition-colors hover:bg-muted/10"
+      className="group relative w-full cursor-pointer rounded-[4px] bg-gradient-to-tr from-border via-border to-[#457AE4] p-[1px]"
       onClick={openLstStrategyDialog}
     >
-      <div className="flex w-full flex-row justify-between">
-        {/* Left */}
-        <LstStrategyHeader strategyType={strategyType} />
+      <div className="relative z-[3] flex flex-col gap-4 rounded-[3px] p-4">
+        <div className="flex w-full flex-row justify-between">
+          {/* Left */}
+          <LstStrategyHeader strategyType={strategyType} />
 
-        {/* Right */}
-        <div className="flex flex-row justify-end gap-6">
-          {/* APR/Max APR */}
-          <div className="flex w-fit flex-col items-end gap-1">
-            <TLabelSans>
-              {!!obligation && hasPosition(obligation) ? "APR" : "Max APR"}
-            </TLabelSans>
-            <TBody className="text-right">{formatPercent(aprPercent)}</TBody>
+          {/* Right */}
+          <div className="flex flex-row justify-end gap-6">
+            {/* APR/Max APR */}
+            <div className="flex w-fit flex-col items-end gap-1">
+              <TLabelSans>
+                {!!obligation && hasPosition(obligation) ? "APR" : "Max APR"}
+              </TLabelSans>
+              <TBody className="text-right">{formatPercent(aprPercent)}</TBody>
+            </div>
           </div>
         </div>
-      </div>
 
-      {!!obligation && hasPosition(obligation) && (
-        <>
-          <Separator />
+        {!!obligation && hasPosition(obligation) && (
+          <>
+            <Separator className="bg-gradient-to-r from-border via-border to-[#457AE4]" />
 
-          <div className="flex w-full flex-col gap-3">
-            {/* Equity */}
-            <LabelWithValue
-              label="Equity"
-              labelTooltip={
-                <>
-                  Equity is calculated as the sum of the net amount deposited,
-                  deposit interest, LST staking yield, and claimed rewards,
-                  minus borrow interest.
-                  <br />
-                  <span className="mt-2 block">
-                    Equity does not include unclaimed rewards.
-                  </span>
-                </>
-              }
-              value={`${formatToken(tvlAmount, { exact: false })} ${defaultCurrencyReserve.token.symbol}`}
-              valueTooltip={`${formatToken(tvlAmount, {
-                dp: defaultCurrencyReserve.token.decimals,
-              })} ${defaultCurrencyReserve.token.symbol}`}
-              horizontal
-            />
-
-            {/* Total PnL */}
-            <PnlLabelWithValue
-              reserve={defaultCurrencyReserve}
-              label="Total PnL"
-              labelTooltip="Total PnL is the difference between the sum of your Equity and unclaimed rewards, and the net amount deposited."
-              pnlAmount={totalPnlAmount}
-              pnlTooltip={
-                realizedPnlAmount === undefined ||
-                totalPnlAmount === undefined ? undefined : (
-                  <div className="flex flex-col gap-2">
-                    {/* Realized PnL */}
-                    <div className="flex flex-row items-center justify-between gap-4">
-                      <TLabelSans>Realized PnL</TLabelSans>
-                      <TBody
-                        className={cn(
-                          realizedPnlAmount.gt(0) && "text-success",
-                          realizedPnlAmount.lt(0) && "text-destructive",
-                        )}
-                      >
-                        {new BigNumber(realizedPnlAmount).eq(0)
-                          ? null
-                          : new BigNumber(realizedPnlAmount).gte(0)
-                            ? "+"
-                            : "-"}
-                        {formatToken(realizedPnlAmount.abs(), {
-                          dp: defaultCurrencyReserve.token.decimals,
-                        })}{" "}
-                        {defaultCurrencyReserve.token.symbol}
-                      </TBody>
-                    </div>
-
-                    {/* Unclaimed rewards */}
-                    <div className="flex flex-row items-center justify-between gap-4">
-                      <TLabelSans>Unclaimed rewards</TLabelSans>
-                      <TBody
-                        className={cn(
-                          unclaimedRewardsAmount.gt(0) && "text-success",
-                          unclaimedRewardsAmount.lt(0) && "text-destructive",
-                        )}
-                      >
-                        {new BigNumber(unclaimedRewardsAmount).eq(0)
-                          ? null
-                          : new BigNumber(unclaimedRewardsAmount).gte(0)
-                            ? "+"
-                            : "-"}
-                        {formatToken(unclaimedRewardsAmount.abs(), {
-                          dp: defaultCurrencyReserve.token.decimals,
-                        })}{" "}
-                        {defaultCurrencyReserve.token.symbol}
-                      </TBody>
-                    </div>
-
-                    <Separator />
-
-                    {/* Total PnL */}
-                    <div className="flex flex-row items-center justify-between gap-4">
-                      <TLabelSans>Total PnL</TLabelSans>
-                      <TBody
-                        className={cn(
-                          totalPnlAmount.gt(0) && "text-success",
-                          totalPnlAmount.lt(0) && "text-destructive",
-                        )}
-                      >
-                        {new BigNumber(totalPnlAmount).eq(0)
-                          ? null
-                          : new BigNumber(totalPnlAmount).gte(0)
-                            ? "+"
-                            : "-"}
-                        {formatToken(totalPnlAmount.abs(), {
-                          dp: defaultCurrencyReserve.token.decimals,
-                        })}{" "}
-                        {defaultCurrencyReserve.token.symbol}
-                      </TBody>
-                    </div>
-                  </div>
-                )
-              }
-            />
-
-            {/* Exposure */}
-            <LabelWithValue
-              label="Leverage"
-              value={`${exposure.toFixed(1)}x`}
-              valueTooltip={`${exposure.toFixed(6)}x`}
-              horizontal
-            />
-
-            {/* Health */}
-            <div className="flex w-full flex-col gap-2">
+            <div className="flex w-full flex-col gap-3">
+              {/* Equity */}
               <LabelWithValue
-                label="Health"
-                value={formatPercent(healthPercent, { dp: 0 })}
+                label="Equity"
+                labelTooltip={
+                  <>
+                    Equity is calculated as the sum of the net amount deposited,
+                    deposit interest, LST staking yield, and claimed rewards,
+                    minus borrow interest.
+                    <br />
+                    <span className="mt-2 block">
+                      Equity does not include unclaimed rewards.
+                    </span>
+                  </>
+                }
+                value={`${formatToken(tvlAmount, { exact: false })} ${defaultCurrencyReserve.token.symbol}`}
+                valueTooltip={`${formatToken(tvlAmount, {
+                  dp: defaultCurrencyReserve.token.decimals,
+                })} ${defaultCurrencyReserve.token.symbol}`}
                 horizontal
               />
 
-              <div className="flex w-full flex-row justify-between">
-                {Array.from({ length: 50 + 1 }).map((_, i, arr) => (
-                  <div
-                    key={i}
-                    className="h-[16px] w-[max(0.5%,2px)] rounded-sm bg-muted/20"
-                    style={{
-                      backgroundColor: healthPercent.gte(
-                        i * (100 / (arr.length - 1)),
-                      )
-                        ? healthColorRange(i / (arr.length - 1)).toString()
-                        : undefined,
-                    }}
-                  />
-                ))}
+              {/* Total PnL */}
+              <PnlLabelWithValue
+                reserve={defaultCurrencyReserve}
+                label="Total PnL"
+                labelTooltip="Total PnL is the difference between the sum of your Equity and unclaimed rewards, and the net amount deposited."
+                pnlAmount={totalPnlAmount}
+                pnlTooltip={
+                  realizedPnlAmount === undefined ||
+                  totalPnlAmount === undefined ? undefined : (
+                    <div className="flex flex-col gap-2">
+                      {/* Realized PnL */}
+                      <div className="flex flex-row items-center justify-between gap-4">
+                        <TLabelSans>Realized PnL</TLabelSans>
+                        <TBody
+                          className={cn(
+                            realizedPnlAmount.gt(0) && "text-success",
+                            realizedPnlAmount.lt(0) && "text-destructive",
+                          )}
+                        >
+                          {new BigNumber(realizedPnlAmount).eq(0)
+                            ? null
+                            : new BigNumber(realizedPnlAmount).gte(0)
+                              ? "+"
+                              : "-"}
+                          {formatToken(realizedPnlAmount.abs(), {
+                            dp: defaultCurrencyReserve.token.decimals,
+                          })}{" "}
+                          {defaultCurrencyReserve.token.symbol}
+                        </TBody>
+                      </div>
+
+                      {/* Unclaimed rewards */}
+                      <div className="flex flex-row items-center justify-between gap-4">
+                        <TLabelSans>Unclaimed rewards</TLabelSans>
+                        <TBody
+                          className={cn(
+                            unclaimedRewardsAmount.gt(0) && "text-success",
+                            unclaimedRewardsAmount.lt(0) && "text-destructive",
+                          )}
+                        >
+                          {new BigNumber(unclaimedRewardsAmount).eq(0)
+                            ? null
+                            : new BigNumber(unclaimedRewardsAmount).gte(0)
+                              ? "+"
+                              : "-"}
+                          {formatToken(unclaimedRewardsAmount.abs(), {
+                            dp: defaultCurrencyReserve.token.decimals,
+                          })}{" "}
+                          {defaultCurrencyReserve.token.symbol}
+                        </TBody>
+                      </div>
+
+                      <Separator />
+
+                      {/* Total PnL */}
+                      <div className="flex flex-row items-center justify-between gap-4">
+                        <TLabelSans>Total PnL</TLabelSans>
+                        <TBody
+                          className={cn(
+                            totalPnlAmount.gt(0) && "text-success",
+                            totalPnlAmount.lt(0) && "text-destructive",
+                          )}
+                        >
+                          {new BigNumber(totalPnlAmount).eq(0)
+                            ? null
+                            : new BigNumber(totalPnlAmount).gte(0)
+                              ? "+"
+                              : "-"}
+                          {formatToken(totalPnlAmount.abs(), {
+                            dp: defaultCurrencyReserve.token.decimals,
+                          })}{" "}
+                          {defaultCurrencyReserve.token.symbol}
+                        </TBody>
+                      </div>
+                    </div>
+                  )
+                }
+              />
+
+              {/* Exposure */}
+              <LabelWithValue
+                label="Leverage"
+                value={`${exposure.toFixed(1)}x`}
+                valueTooltip={`${exposure.toFixed(6)}x`}
+                horizontal
+              />
+
+              {/* Health */}
+              <div className="flex w-full flex-col gap-2">
+                <LabelWithValue
+                  label="Health"
+                  value={formatPercent(healthPercent, { dp: 0 })}
+                  horizontal
+                />
+
+                <div className="flex w-full flex-row justify-between">
+                  {Array.from({ length: 50 + 1 }).map((_, i, arr) => (
+                    <div
+                      key={i}
+                      className="h-[16px] w-[max(0.5%,2px)] rounded-sm bg-muted/20"
+                      style={{
+                        backgroundColor: healthPercent.gte(
+                          i * (100 / (arr.length - 1)),
+                        )
+                          ? healthColorRange(i / (arr.length - 1)).toString()
+                          : undefined,
+                      }}
+                    />
+                  ))}
+                </div>
               </div>
             </div>
-          </div>
-        </>
-      )}
+          </>
+        )}
+      </div>
+
+      <div
+        className="absolute inset-px z-[2] rounded-[3px] opacity-40 transition-opacity group-hover:opacity-50"
+        style={{
+          backgroundImage: `url('${ASSETS_URL}/strategies/card-bg.png')`,
+          backgroundPosition: "top right",
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+        }}
+      />
+
+      <div className="absolute inset-px z-[1] rounded-[3px] bg-card" />
     </div>
   );
 }
