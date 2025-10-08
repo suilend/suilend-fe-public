d8a08e44270c60499dba3b1491662f4c73f67976
0xshiba1 <158560741+0xshiba1@users.noreply.github.com>
2025-10-08 14:44:06 +0800

    fix

--- frontend/src/components/shared/TokenSelectionDialog.tsx
+++ frontend/src/components/shared/TokenSelectionDialog.tsx
@@ -15,7 +15,6 @@ import {
 
 import {
   NORMALIZED_SEND_COINTYPE,
-  NORMALIZED_STRAT_COINTYPE,
   NORMALIZED_SUI_COINTYPE,
   NORMALIZED_USDC_COINTYPE,
   NORMALIZED_WAL_COINTYPE,
@@ -104,7 +103,9 @@ function TokenRow({
                     Object.keys(_appData.reserveMap).includes(token.coinType),
                 ) ||
                 verifiedCoinTypes.includes(token.coinType) ||
-                [NORMALIZED_STRAT_COINTYPE].includes(token.coinType) ? (
+                [
+                  "0x8556539cf20b8640738d919ce3fe9d79b982f7d14a0861b650ff24b3cbd80e73::strat::STRAT",
+                ].includes(token.coinType) ? (
                   <Tooltip
                     title={
                       Object.values(allAppData.allLendingMarketData).find(
