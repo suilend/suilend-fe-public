d89766bb8e3c6c0e3baf254c54cc89842a64b95f
0xshiba1 <158560741+0xshiba1@users.noreply.github.com>
2025-10-11 13:48:00 +0800

    add back bulk swap, admin page

--- frontend/src/components/admin/thirdPartyFees/ThirdPartyFeesTab.tsx
+++ frontend/src/components/admin/thirdPartyFees/ThirdPartyFeesTab.tsx
@@ -1,12 +1,26 @@
+import { LENDING_MARKET_ID } from "@suilend/sdk";
+import { NORMALIZED_SUI_COINTYPE, getToken } from "@suilend/sui-fe";
+
 import CetusCard from "@/components/admin/thirdPartyFees/CetusCard";
 import RootletsCard from "@/components/admin/thirdPartyFees/RootletsCard";
 import SuilendCapsulesCard from "@/components/admin/thirdPartyFees/SuilendCapsulesCard";
+import BulkSwapCard from "@/components/swap/BulkSwapCard";
+import { useLoadedAppContext } from "@/contexts/AppContext";
 
 export default function ThirdPartyFeesTab() {
+  const { allAppData } = useLoadedAppContext();
+  const appDataMainMarket = allAppData.allLendingMarketData[LENDING_MARKET_ID];
+
   return (
     <div className="flex w-full flex-col gap-2">
       <SuilendCapsulesCard />
       <RootletsCard />
+      <BulkSwapCard
+        tokenOut={getToken(
+          NORMALIZED_SUI_COINTYPE,
+          appDataMainMarket.coinMetadataMap[NORMALIZED_SUI_COINTYPE],
+        )}
+      />
       <CetusCard />
     </div>
   );
