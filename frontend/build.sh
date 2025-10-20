139d176c9358bb76d85f59e0a627f4eb058c52ec
0xshiba1 <158560741+0xshiba1@users.noreply.github.com>
2025-10-20 13:29:33 +0800

    update build.sh files

--- frontend/build.sh
+++ frontend/build.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-echo "Checking if frontend has changed..."
-
-# If frontend has changes in the last commit range, proceed with build
+# Check if frontend/ has changes in the last commit range
 if git diff --quiet HEAD^ HEAD -- . ../sdk/ ../.gitignore ../bun.lock ../eslint.config.js ../package.json ../README.md; then
-  echo "No changes in frontend/. Skipping build."
+  # Skip build
+  echo "🛑 - No changes in frontend/ - skipping build"
   exit 0
 else
-  echo "Changes detected in frontend/. Proceeding with build."
-  exit 1
+  # Check if environment is production, beta, or playground
+  if [[ "$VERCEL_ENV" == "production" || "$VERCEL_ENV" == "beta" || "$VERCEL_ENV" == "playground" ]] ; then
+    # Proceed with build
+    echo "🎉 - Changes detected in frontend/, and environment is production, beta, or playground - proceeding with build"
+    exit 1;
+  else
+    # Skip build
+    echo "🛑 - Environment is not production, beta, or playground - skipping build"
+    exit 0;
+  fi
 fi
+
--- send/build.sh
+++ send/build.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-echo "Checking if send has changed..."
-
-# If send has changes in the last commit range, proceed with build
+# Check if send/ has changes in the last commit range
 if git diff --quiet HEAD^ HEAD -- .; then
-  echo "No changes in send/. Skipping build."
+  # Skip build
+  echo "🛑 - No changes in send/ - skipping build"
   exit 0
-
-elif [[ "$VERCEL_ENV" == "beta" || "$VERCEL_ENV" == "production" || "$VERCEL_ENV" == "playground" ]] ; then
-# Proceed with the build
-  echo "✅ - Build can proceed"
-exit 1;
 else
-  # Don't build
-  echo "🛑 - Build cancelled"
-  exit 0;
+  # Check if environment is production, beta, or playground
+  if [[ "$VERCEL_ENV" == "production" || "$VERCEL_ENV" == "beta" || "$VERCEL_ENV" == "playground" ]] ; then
+    # Proceed with build
+    echo "🎉 - Changes detected in send/, and environment is production, beta, or playground - proceeding with build"
+    exit 1;
+  else
+    # Skip build
+    echo "🛑 - Environment is not production, beta, or playground - skipping build"
+    exit 0;
+  fi
 fi
+
