88732e8927b49b6dc6a2e093d8c4fc5e3522cf9d
0xshiba1 <158560741+0xshiba1@users.noreply.github.com>
2025-12-15 13:44:09 +0800

    fix headers

--- send/next.config.js
+++ send/next.config.js
@@ -32,16 +32,21 @@ module.exports = {
   async headers() {
     return [
       {
-        key: "X-Frame-Options",
-        value: "DENY",
-      },
-      {
-        key: "X-Strict-Transport-Security",
-        value: "max-age=31536000",
-      },
-      {
-        key: "X-Content-Type-Options",
-        value: "nosniff",
+        source: "/:path*",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=31536000",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+        ],
       },
     ];
   },
